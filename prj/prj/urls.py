33c7ab4168dd83c7f935f6c6f57dfda344078474
Yasin <ebrahimiy07@gmail.com>
2024-09-11 16:42:21 +0330

    ye seri kar jizi

--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -19,9 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', include('home.urls', namespace='home')),
 
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls', namespace='accounts'))
 
 
 ]
