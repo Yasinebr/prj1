0e5f5d726cea945051fefc6990069b5bb1187cad
Yasin <ebrahimiy07@gmail.com>
2024-09-13 16:12:44 +0330

    taghirat

--- prj/accounts/views.py
+++ prj/accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,redirect
 from django.views import View
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 
 class RegisterView(View):
@@ -17,8 +18,27 @@ class RegisterView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
             messages.success(request, 'user create successfully', 'success')
             return redirect('home:home')
         return render(request, self.template_name, {'form': form})
+
+class LoginView(View):
+    form_class = UserLoginForm
+    template_name = 'accounts/login.html'
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                messages.success(request, 'Login Successfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'username or password is wring', 'warning')
+        return render(request, self.template_name, {'form':form})
 
