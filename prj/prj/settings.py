011be7b43309b98752fca9c3db56b75b69d5a3f2
Yasin <ebrahimiy07@gmail.com>
2024-10-30 15:14:05 +0330

    change1

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9tx#46!%bm@0hyld8)!$v=mk=kx_$jrs)xov%ib*y@hg@h9vb0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
